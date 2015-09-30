2cc41a0d2548bbc4178cb0d5205f2d1514f3ad28
Larry Smith Jr <mrlesmithjr@gmail.com>
2015-09-30 00:39:30 -0400

    added Windows host check and updated script

--- bootstrap_ansible.sh
+++ bootstrap_ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get install -y python-pip python-dev
-pip install ansible==1.9.1
+pip install ansible
 
-ansible-galaxy install mrlesmithjr.bootstrap
-ansible-galaxy install mrlesmithjr.base
+cd /vagrant
+ansible-playbook -i hosts bootstrap.yml
